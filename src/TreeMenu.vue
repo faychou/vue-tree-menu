9f01744509fb5eb50378d1cbb20aaa44f2b803f1
faychou <fay_chou@163.com>
2017-11-12 20:55:16 +0800

    基本功能实现

---
+++ src/TreeMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tree-menu">
+    <div :style="indent" @click="toggleChildren">{{ label }}</div>
+    <tree-menu 
+      v-if="showChildren" 
+      v-for="node in nodes" 
+      :nodes="node.nodes" 
+      :label="node.label" 
+      :depth="depth + 1"
+    >
+    </tree-menu>
+  </div>
+</template>
+
+<script>
+  export default { 
+    props: [ 'label', 'nodes', 'depth' ],
+    name: 'tree-menu',
+    data() {
+      return { showChildren: false }
+    },
+    computed: {
+      indent() {
+        return { transform: `translate(${this.depth * 50}px)` }
+      }
+    },
+    methods: {
+      toggleChildren() {
+        this.showChildren = !this.showChildren;
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
 import Vue from 'vue'
-import App from './App.vue'
+import TreeMenu from './TreeMenu.vue'
+
+//定义树形菜单数据
+let tree = {
+  label: 'root',
+  nodes: [
+    {
+      label: 'item1',
+      nodes: [
+        {
+          label: 'item1.1'
+        },
+        {
+          label: 'item1.2',
+          nodes: [
+            {
+              label: 'item1.2.1'
+            }
+          ]
+        }
+      ]
+    }, 
+    {
+      label: 'item2'  
+    }
+  ]
+}
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  // render: h => h(App)
+  data: {
+    tree
+  },
+  components: {
+    TreeMenu
+  }
 })
